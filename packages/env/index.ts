03f57633b0332a39fa00ef4c50d0391ca740e04b
Marco Santos <marco@marcosantos.dev>
2024-12-11 02:19:35 -0300

    feat(env): add emptyStringAsUndefined option to createEnv

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -95,4 +95,5 @@ export const env = createEnv({
     NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL:
       process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
   },
+  emptyStringAsUndefined: true,
 });
